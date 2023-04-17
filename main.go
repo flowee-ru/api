058a09ea48bfc9625354c662e3c0ffd18679c573
Kirill <87936030+MisterKirill@users.noreply.github.com>
2023-04-17 16:14:24 +0500

    gorilla mux router

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -33,30 +34,34 @@ func main() {
 		log.Fatalln(err)
 	}
 
+	router := mux.NewRouter()
+
 	// auth
-	http.HandleFunc(basePath + "/auth/login", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(basePath + "/auth/login", func(w http.ResponseWriter, r *http.Request) {
 		routes.Login(w, r, db, ctx)
 	})
-	http.HandleFunc(basePath + "/auth/register", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(basePath + "/auth/register", func(w http.ResponseWriter, r *http.Request) {
 		routes.Register(w, r, db, ctx)
 	})
-	http.HandleFunc(basePath + "/auth/verifyAccount", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(basePath + "/auth/verifyAccount", func(w http.ResponseWriter, r *http.Request) {
 		routes.VerifyAccount(w, r, db, ctx)
 	})
-	http.HandleFunc(basePath + "/auth/verifyToken", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(basePath + "/auth/verifyToken", func(w http.ResponseWriter, r *http.Request) {
 		routes.VerifyToken(w, r, db, ctx)
 	})
-	http.HandleFunc(basePath + "/auth/resendEmail", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(basePath + "/auth/resendEmail", func(w http.ResponseWriter, r *http.Request) {
 		routes.ResendEmail(w, r, db, ctx)
 	})
 
 	// actions
-	http.HandleFunc(basePath + "/actions/follow", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(basePath + "/actions/follow", func(w http.ResponseWriter, r *http.Request) {
 		routes.Follow(w, r, db, ctx)
 	})
-	http.HandleFunc(basePath + "/actions/unfollow", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc(basePath + "/actions/unfollow", func(w http.ResponseWriter, r *http.Request) {
 		routes.Unfollow(w, r, db, ctx)
 	})
+
+	http.Handle("/", router)
 
 	log.Println("Starting server on port " + port)
 	http.ListenAndServe(":" + port, nil)
